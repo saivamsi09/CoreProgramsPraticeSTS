59ba76bb1d6bacf13f8eede98d82e5fc674453cd
saivamsi <50793524+saivamsi09@users.noreply.github.com>
2021-05-03 09:57:10 +0530

    added another method

--- SimpleCoreprojects/src/StringManupulationsJava.java
+++ SimpleCoreprojects/src/StringManupulationsJava.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 
  */
@@ -12,7 +15,8 @@ public class StringManupulationsJava {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String string= "vamsi";
+		String string= "vamsivamsi";
+		findDuplicates(string);
 		System.out.println(reverseaString(string));
 
 	}
@@ -21,9 +25,33 @@ public class StringManupulationsJava {
 		StringBuffer sBuffer = new StringBuffer();
 		sBuffer.append(str);
 		sBuffer.reverse();
-		
-		
 		return sBuffer;
+		
+		
+	}
+	public static void findDuplicates(String str) {
+		 HashMap<Character, Integer> count = new HashMap<>();
+	        /*Store duplicates present
+	        in the passed string */
+	        for (int i = 0; i < str.length(); i++) {
+	            if (!count.containsKey(str.charAt(i)))
+	                count.put(str.charAt(i), 1);
+	            else
+	                count.put(str.charAt(i),
+	                          count.get(str.charAt(i)) + 1);
+	        }
+	 
+	        /*Print duplicates in sorted order*/
+	        System.out.println(count);
+	        for (Map.Entry mapElement : count.entrySet()) {
+	            char key = (char)mapElement.getKey();
+	            int value = ((int)mapElement.getValue());
+	 
+	            if (value > 1)
+	                System.out.println(key
+	                                   + ", count = " + value);
+	        }
+		
 		
 		
 	}
